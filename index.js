bd0aa49e209033e1edaa3cd84d684bbddcf72192
ibrahimahmedoglu <ibrahim.ahmedoglu@bahcesehir.edu.tr>
2023-06-30 14:00:25 +0300

    completed assigntment

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  Pseudocode:
+  - Initialize a startIndex variable to 0.
+  - Iterate until the startIndex is less than the length of the word divided by 2.
+  - Inside the loop, calculate the endIndex as word.length - 1 - startIndex.
+  - Compare the characters at the startIndex and endIndex.
+  - If they are not equal, return false (not a palindrome).
+  - If the loop completes without finding any unequal characters, return true (palindrome).
 */
 
 /*
-  Add written explanation of your solution here
+  Solution Explanation:
+  - The function iterates through the characters of the word using a for loop.
+  - It compares the character at the current startIndex with the character at the corresponding endIndex.
+  - If any pair of characters is not equal, the function immediately returns false, indicating that the word is not a palindrome.
+  - If the loop completes without finding any unequal characters, the function returns true, indicating that the word is a palindrome.
 */
 
 // You can run `node index.js` to view these console logs
